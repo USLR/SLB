df8e2989de670774dbeef695fa28b50a686eb8f2
PizzaCrust <pizzacrustcoder@gmail.com>
2017-09-15 19:31:16 +0700

    move to game instances and  add no servers notification

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendFunc){
     var json = JSON.parse(request)
     // we have servers
     // add buttons
-    var parent = document.getElementsByClassName("game-stat-footer")  
+    var parent = document.getElementById("game-instances")  
     var titleNode = document.createElement("H1")
     var titleText = document.createTextNode("SLS Server List")
     titleNode.appendChild(titleText)
@@ -25,15 +25,23 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendFunc){
     parent[0].appendChild(titlebreak1)
     parent[0].appendChild(titleNode)
     parent[0].appendChild(titlebreak2)
-    for (var index = 0; index < json.length; index++) {
-      var element = json[index];
-      console.log(element)
-      var button = document.createElement("button")
-      button.setAttribute("OnClick", "Roblox.GameLauncher.joinGameInstance(" + element.placeId + ", \"" + element.serverId + "\")")
-      var linebreak = document.createElement("br")
-      var text = document.createTextNode("Press to join server! (amount of players: " + element.registeredPlayerCount + ")")
-      button.appendChild(text)
-      parent[0].appendChild(button)
-      parent[0].appendChild(linebreak)      
+    if (json.length == 0) {
+      console.log("empty server list")
+      var notifyNode = document.createElement("H3")
+      var notifyText = document.createTextNode("No servers are currently running or this game does not use SLS or is not in the repository.")
+      notifyNode.appendChild(notifyText)
+      parent[0].appendChild(notifyNode)
+    } else {
+      for (var index = 0; index < json.length; index++) {
+        var element = json[index];
+        console.log(element)
+        var button = document.createElement("button")
+        button.setAttribute("OnClick", "Roblox.GameLauncher.joinGameInstance(" + element.placeId + ", \"" + element.serverId + "\")")
+        var linebreak = document.createElement("br")
+        var text = document.createTextNode("Press to join server! (amount of players: " + element.registeredPlayerCount + ")")
+        button.appendChild(text)
+        parent[0].appendChild(button)
+        parent[0].appendChild(linebreak)      
+      }
     }
 })
